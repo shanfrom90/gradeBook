03fba8ada50c1291670d5262c4c26cae3e64fe01
Shannon Adams <77690999+shanfrom90@users.noreply.github.com>
2021-04-16 22:38:19 -0400

    experimented with numbers array vs List

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,6 +7,20 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            //arrays need to have a specified size, this is why data structures like list is a good option
+            double [] arrayGrades = new [] {12.7, 34.1, 38.6};
+
+             //lists are dynamic
+            List<double> grades = new List<double>();
+            grades.Add(56.1);
+             
+            var result = 0.0;
+            foreach(double grade in grades){
+                result += grade;
+            }
+
+            result /= grades.Count;
+            System.Console.WriteLine($"The average grade is: {result:N2}");
             if(args.Length > 0){
 
                 Console.WriteLine($"Hello, {args[0]}!");
