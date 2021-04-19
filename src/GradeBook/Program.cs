7893cb5c8a69c2ab7a6eb18590976e33b2abf23c
Shannon Adams <77690999+shanfrom90@users.noreply.github.com>
2021-04-18 20:38:10 -0400

    Added book and statistics classes. Started writing unit tests

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,27 +7,27 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+
+            var book = new Book("Shannon's Grade Book");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            var stats = book.GetStatistics();
+
+            System.Console.WriteLine($"The average grade is: {stats.Average:N2}");
+            System.Console.WriteLine($"The lowest grade is: {stats.Low}");
+            System.Console.WriteLine($"The highest grade is: {stats.High}");   
+
+
             //arrays need to have a specified size, this is why data structures like list is a good option
-            double [] arrayGrades = new [] {12.7, 34.1, 38.6};
+            // double [] arrayGrades = new [] {12.7, 34.1, 38.6};
 
              //lists are dynamic
-            List<double> grades = new List<double>();
-            grades.Add(56.1);
+            // List<double> grades = new List<double>(){12.7, 34.1, 38.6};
+            // grades.Add(56.1);
              
-            var result = 0.0;
-            foreach(double grade in grades){
-                result += grade;
-            }
-
-            result /= grades.Count;
-            System.Console.WriteLine($"The average grade is: {result:N2}");
-            if(args.Length > 0){
-
-                Console.WriteLine($"Hello, {args[0]}!");
-            }
-            else{
-                System.Console.WriteLine("Hello!");
-            }
+           
+             
         }
     }
 }
